a2d5d24ad853b456ad9674e1244ec1c909fbafb6
Cesar-M-Diaz <arq.cmdiaz1@gmail.com>
2021-09-06 21:47:49 -0500

    Make connection with mongodb atlas

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const cors = require('cors');
-const routes = require('./routes');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
-app.use(routes);
+const uri =
+  'mongodb+srv://Cesar-Diaz:vacivusVoid1%40@cluster0.ybria.mongodb.net/Agora?retryWrites=true&w=majority';
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established succesfully');
+});
 
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(500).json({ error: err.message });
 });
 
-app.listen(3001, () => console.log('Servidor corriendo ...'));
+app.listen(3001, () => console.log('Server running ...'));
