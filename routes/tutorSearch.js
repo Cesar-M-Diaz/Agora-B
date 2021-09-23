39b5e7062f82ff4a60b6d8383749d46f8733bef9
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-09-23 11:46:11 -0500

    send information in pages

--- routes/tutorSearch.js
+++ routes/tutorSearch.js
@@ -3,7 +3,7 @@ const Controller = require("../controllers/tutorSearch.controller")
 
 const app = express.Router();
 
-app.get('/tutorsearch/:focus', Controller.findTutors );
+app.get('/tutorsearch/:focus/:page', Controller.findTutors );
 
 
 module.exports = app;
