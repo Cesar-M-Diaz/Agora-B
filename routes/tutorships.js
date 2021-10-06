b70d30c0b4d6d62b4f0c784512a54ed61ede30c3
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-10-06 17:02:39 -0500

    Get tutorships done

--- routes/tutorships.js
+++ routes/tutorships.js
@@ -4,5 +4,6 @@ const tutorshipController = require('../controllers/tutorship.controller');
 const tutorship = express.Router();
 
 tutorship.post('/tutorship', tutorshipController.createTutorship);
+tutorship.get('/tutorships', tutorshipController.getTutorships);
 
 module.exports = tutorship;
