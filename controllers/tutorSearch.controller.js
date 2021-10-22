022356dedbef18bc19e9d6e3bf6ae10352d6c5fc
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-10-22 00:13:46 -0500

    search by name and category

--- controllers/tutorSearch.controller.js
+++ controllers/tutorSearch.controller.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose')
 const Tutor = require("../models/tutor.model");
 
-
 const findTutors = async (req, res) => {
+
+  
+  
   try {
+    const query = req.params.query
     const page = req.params.page || 1
-    const count = await Tutor.count({focus: req.params.focus})
-    const data = await Tutor.find({focus: req.params.focus},
+    const regEx = new RegExp(query,"i")
+    const count = await Tutor.count({$or:[{"focus": regEx}, {"name":{"$in":regEx }}]})
+    const data = await Tutor.find({$or:[{"focus": regEx}, {"name":{"$in":regEx }}]},
       ['name', 'profile_photo', 'profession', 'focus', 'rating', 'description','price'])
       .sort({rating: -1})
       .skip((page-1)*9)
