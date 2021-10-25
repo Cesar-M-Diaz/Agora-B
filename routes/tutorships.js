ba8659c694399080b3ce678bea07159403d27f8a
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-10-25 09:45:24 -0500

    Complete tutorship

--- routes/tutorships.js
+++ routes/tutorships.js
@@ -8,5 +8,7 @@ tutorship.post('/tutorship', tutorshipController.createTutorship);
 tutorship.get('/tutorships/:id', tutorshipController.getTutorships);
 tutorship.post('/cancelTutorship', verifyAuth, tutorshipController.cancelTutorship);
 tutorship.get('/tutorship/:id', tutorshipController.getTutorship);
+tutorship.post('/completeTutorship', verifyAuth, tutorshipController.completeTutorship);
+
 
 module.exports = tutorship;
